7543e963e10770ee5fdb66a43e8c3e5039b39bdf
azougho-coder <azougho@gmail.com>
2026-07-19 15:34:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.shortflow.net",
+          },
+        ],
+        destination: "https://shortflow.net/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
+
 module.exports = nextConfig;
